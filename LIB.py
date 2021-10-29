915a7ad04968b983b811af71ed1a2d96aff9061e
JoseOrtega870 <74004807+JoseOrtega870@users.noreply.github.com>
2021-10-29 11:39:29 -0500

    Update LIB.py

--- LIB.py
+++ LIB.py
@@ -19,7 +19,48 @@ def laplacianOfGaussian(sigma, K):
                         * numpy.exp(-(x**2+y**2)/(2*sigma**2))
     return M
 
-def convolucion(imagen, kernel, k1): #Convolucion de imagen
+def met_sobel(image, size, verbose=False): #Kernel Sobel
+    vertical = np.array([[1],[2],[1]])
+    horizontal = np.array([[1,0,-1]])
+    filter3 = vertical*horizontal
+    if size == 3:
+        filter = filter3
+    else:
+        filter = filter3
+        while size > 3:
+            horizontal2 = np.array([[1,2,1]])
+            filter = signal.convolve2d(vertical*horizontal2, filter)
+            print("Kernel  Metodo Sobel : ")
+            print(filter)
+            size = size - 2
+    
+    #Convolucion 
+    disp1 = convolution(image, filter, verbose)
+
+    if verbose:
+        plt.imshow(disp1, cmap='gray')
+        plt.title("Horizontal Edge")
+        plt.show()
+
+    disp2 = convolution(image, np.flip(filter.T, axis=0), verbose)
+
+    if verbose:
+        plt.imshow(disp2, cmap='gray')
+        plt.title("Vertical Edge")
+        plt.show()
+  
+    gradient_magnitude = np.sqrt(np.square(disp1) + np.square(disp2))
+
+    gradient_magnitude *= 255.0 / gradient_magnitude.max()
+    
+    if verbose:
+        plt.imshow(gradient_magnitude, cmap='gray')
+        plt.title("Gradient Magnitude")
+        plt.show()
+
+    return gradient_magnitude
+
+def convolucion(imagen, kernel, k1, k2): #Convolucion de imagen
         Is = Image.open(imagen)
         I = Is.convert('L')
         I = numpy.asarray(I)
@@ -32,6 +73,7 @@ def convolucion(imagen, kernel, k1): #Convolucion de imagen
         
         j0 = ndimage.convolve(I, kernel, mode='constant', cval=0.0)
 	j1 = ndimage.convolve(I, k1, mode='constant', cval=0.0)
+	j1 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
         
         plt.figure(figsize=(15,15)) #Graficas
         plt.subplot(3,3,1)
@@ -45,6 +87,10 @@ def convolucion(imagen, kernel, k1): #Convolucion de imagen
 	plt.subplot(3,3,3)
         plt.imshow(j1)
         plt.xlabel('laplacian Of Gaussian')
+	
+	plt.subplot(3,3,4)
+        plt.imshow(j2)
+        plt.xlabel('Sobel')
         
         plt.grid(False)
         plt.show()
